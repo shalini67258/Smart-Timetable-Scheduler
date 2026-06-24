e8c35107b85406f0aa47f310ca6f0d89104aa3b2
shalini67258 <shalinialigetip@gmail.com>
2026-06-24 16:39:11 +0530

    Update Classroom.java

--- timetable/src/main/java/com/drk/timetable/model/Classroom.java
+++ timetable/src/main/java/com/drk/timetable/model/Classroom.java
@@ -16,8 +16,11 @@ public class Classroom {
         this.roomNumber = roomNumber;
         this.capacity = capacity;
     }
-    // Getters and Setters
-    public Long getId() { return id; } public void setId(Long id) { this.id = id; }
-    public String getRoomNumber() { return roomNumber; } public void setRoomNumber(String roomNumber) { this.roomNumber = roomNumber; }
-    public int getCapacity() { return capacity; } public void setCapacity(int capacity) { this.capacity = capacity; }
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+    public String getRoomNumber() { return roomNumber; }
+    public void setRoomNumber(String roomNumber) { this.roomNumber = roomNumber; }
+    public int getCapacity() { return capacity; }
+    public void setCapacity(int capacity) { this.capacity = capacity; }
 }
