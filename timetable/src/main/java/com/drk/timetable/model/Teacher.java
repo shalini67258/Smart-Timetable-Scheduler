154d37acd01ea3d5fb86fa41ef4a5d24dce88c00
shalini67258 <shalinialigetip@gmail.com>
2026-06-24 16:38:32 +0530

    Update Teacher.java

--- timetable/src/main/java/com/drk/timetable/model/Teacher.java
+++ timetable/src/main/java/com/drk/timetable/model/Teacher.java
@@ -18,9 +18,13 @@ public class Teacher {
         this.department = department;
         this.email = email;
     }
-    // Getters and Setters
-    public Long getId() { return id; } public void setId(Long id) { this.id = id; }
-    public String getName() { return name; } public void setName(String name) { this.name = name; }
-    public String getEmail() { return email; } public void setEmail(String email) { this.email = email; }
-    public String getDepartment() { return department; } public void setDepartment(String department) { this.department = department; }
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+    public String getDepartment() { return department; }
+    public void setDepartment(String department) { this.department = department; }
 }
